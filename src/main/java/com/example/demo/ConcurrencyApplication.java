d3f80e7075f6c023d9c6cf297a34eac0782db75e
imooc <359625410@qq.com>
2018-07-10 15:00:11 +0800

    first

---
+++ src/main/java/com/example/demo/ConcurrencyApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class ConcurrencyApplication extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+
+
+}
